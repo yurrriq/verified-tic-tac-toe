2d98e3912fc8f02ea7e344230a238afc0ee9746e
Eric Bailey <eric@ericb.me>
2021-07-05 12:47:03 -0500

    nix flake init

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/release-21.05";
+  };
+
+  outputs = { self, emacs-overlay, nixpkgs }:
+    let
+      pkgs = import nixpkgs {
+        overlays = [
+          emacs-overlay.overlay
+          (
+            final: prev: {
+              myEmacs = prev.emacsWithPackagesFromUsePackage {
+                alwaysEnsure = true;
+                config = ./init.el;
+              };
+            }
+          )
+        ];
+        system = "x86_64-linux";
+      };
+    in
+    {
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cabal-install
+          cargo # NOTE: needed for nixpkgs-fmt pre-commit hook
+          ghcid
+          haskell-language-server
+          haskellPackages.ormolu
+          haskellPackages.pointfree
+          hlint
+          idris2
+          gitAndTools.pre-commit
+          myEmacs
+          nixpkgs-fmt
+        ];
+      };
+    };
+}
